c7c0751af9950abecf6fbc6f2ce0743ef4d1d4f3
man0405 <vanman160504@gmail.com>
2025-03-13 11:13:17 +0700

    feat: change output configuration to standalone for static exports

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	output: "export", // <=== enables static exports
+	output: "standalone", // <=== enables static exports
 	basePath: "/",
 	reactStrictMode: true,
 	images: {
