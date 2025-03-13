8bde881587c98b4b8b6c299ecc10b65992858a80
man0405 <vanman160504@gmail.com>
2025-03-13 10:55:13 +0700

    feat: set basePath to '/' in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: "export", // <=== enables static exports
+	basePath: "/",
 	reactStrictMode: true,
 	images: {
 		domains: ["ui-avatars.com"],
